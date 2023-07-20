62058503a226513112f8f2c9bb320a5c70857fdf
Assem Ashraf <assemdev0@gmail.com>
2023-07-20 11:09:13 +0300

    add extension

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -22,3 +22,39 @@ class SliderViewObject {
     required this.currentIndex,
   });
 }
+
+// Login models
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer({
+    required this.id,
+    required this.name,
+    required this.numOfNotifications,
+  });
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+
+  Contacts({
+    required this.phone,
+    required this.email,
+    required this.link,
+  });
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication({
+    required this.contacts,
+    required this.customer,
+  });
+}
